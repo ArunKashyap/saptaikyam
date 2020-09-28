8ec2e42746be098eac61f212b178e49358b1b31d
AK <kacchy@gmail.com>
2020-09-28 13:42:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Panorama Sapthaikyam</h1>
+      <h2>7 Towers... One Family..</h2>
+    </div>
+  );
+}
