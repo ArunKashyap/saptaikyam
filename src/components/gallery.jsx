18b8144aa269c8b48a3eaa9f2afd763a61e05ae4
kacchy@gmail.com <kacchy@gmail.com>
2020-10-02 21:38:21 +0530

    videos-withoutcache

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -20,8 +20,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Saptaikyam Intro Video</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/intro-bg.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/Saptaikyam.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/intro-bg.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/Saptaikyam.mp4"
               fluid={false}
               width={380}
               height={220}
@@ -35,8 +35,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Parvati Chauhan</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/01.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/01.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/01.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/01.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -50,8 +50,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Vekataramana</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/02.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/02.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/02.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/02.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -65,8 +65,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Sujatha Rao</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/03.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/03.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/03.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/03.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -80,8 +80,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Amit Surana</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/04.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/04.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/04.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/04.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -95,8 +95,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Girish G</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/05.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/05.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/05.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/05.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -110,8 +110,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Shivkumar Ramesh</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/06.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/06.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/06.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/06.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -125,8 +125,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Prasad T</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/07.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/07.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/07.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/07.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -140,8 +140,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Puneet Rawal</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/09.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/09.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/09.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/09.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -155,8 +155,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Dr Vijay Joshi</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/10.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/10.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/10.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/10.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -170,8 +170,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Vishwanath Hosatti</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/11.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/11.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/11.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/11.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -185,8 +185,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Vinod Madanshetti</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/12.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/12.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/12.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/12.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -200,8 +200,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Jitendra D J</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/13.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/13.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/13.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/13.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -215,8 +215,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Harsha Kashyap</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/14.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/14.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/14.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/14.mp4"
                fluid={false}
                width={380}
                height={220}
@@ -230,8 +230,8 @@ export class Gallery extends Component {
             <h5 className="card-title">Raghavendra Bhat</h5>
             <Player
               playsInline
-              poster="https://d1cw6765f5rr71.cloudfront.net/15.jpg"
-              src="https://d1cw6765f5rr71.cloudfront.net/15.mp4"
+              poster="https://saptaikyam.s3.amazonaws.com/15.jpg"
+              src="https://saptaikyam.s3.amazonaws.com/15.mp4"
                fluid={false}
                width={380}
                height={220}
